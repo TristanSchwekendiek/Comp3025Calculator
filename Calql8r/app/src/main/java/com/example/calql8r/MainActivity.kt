af9f76b0b1225d5477fdef560ae60fd671cf3dce
TristanSchwekendiek <60010974+TristanSchwekendiek@users.noreply.github.com>
2024-02-04 23:46:26 -0500

    Add files via upload
    init commit

---
+++ Calql8r/app/src/main/java/com/example/calql8r/MainActivity.kt
@@ -0,0 +1,18 @@
+
+// File Name: calql8r
+// Author Name: Tristan Schwekendiek
+// Student ID: 1200799
+// Date: February 4, 2024
+// Calculator Application
+// Version 2
+
+package com.example.calql8r
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
